f18134fc0a0dd6cd06cbb5b10e37da985045e9ad
ZHWANG74 <114205631+ZHWANG74@users.noreply.github.com>
2022-12-22 13:31:20 -0500

    Create exercise1.java

---
+++ Object-oriented/exercise1.java
@@ -0,0 +1,9 @@
+Demand:
+Analyse:
+Implement:
+
+1.Introduction of keywords "static", and take clss of "person" as an example to explain.
+If multiple objects have the same member variable, we could use "static" to modify.
+  Keyword "static":
+    1. Can modify member variables and member methods
+    2. 
